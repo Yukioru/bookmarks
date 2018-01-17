c7e23878b40c0d6d011cecd7a33bdbaa3d4a76d5
Yukioru <obt195@gmail.com>
2018-01-17 12:48:06 +0400

    Remote link

--- vscode-starter-kit/simple-start.sh
+++ vscode-starter-kit/simple-start.sh
@@ -8,7 +8,7 @@ unzip -q /tmp/vscode.zip -d /Applications
 sleep 1
 export PATH="$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin"
 
-sh ./install-extensions.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Yukioru/bookmarks/master/vscode-starter-kit/install-extensions.sh)"
 
 echo "Удаление ненужных файлов..."
 rm -rf /tmp/vscode.zip
